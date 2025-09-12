bf0931322fa01423939ff878383ccb16c60cb797
klee133 <61660035+klee133@users.noreply.github.com>
2025-09-12 14:19:16 -0400

    Create repeatString() function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return "Number has to be higher than 0";
+    let ans = '';
+    for(let i = num; i > 0; i--) {
+        ans += string;
+    }
+    return ans;
 };
+
+console.log(repeatString('hey', 3));
+console.log(repeatString('hey', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
