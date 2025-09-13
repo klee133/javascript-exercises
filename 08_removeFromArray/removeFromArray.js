ed95b01941f467fd69d7a3938ec33727441e84cf
klee133 <61660035+klee133@users.noreply.github.com>
2025-09-12 22:18:28 -0400

    Create removeFromArray() function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) { 
+    return arr.filter((element) => {
+        for(item of args) {
+            if(item === element) {  return false;  }
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
