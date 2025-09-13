1345294091d1274a9b347c38d3d7476a1976a783
klee133 <61660035+klee133@users.noreply.github.com>
2025-09-13 14:09:48 -0400

    Create sumAll() function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)) {  return "ERROR"  }
+    if(int1 < 0 || int2 < 0) {  return "ERROR"  }
+    
+    const start = (int1 < int2) ? int1 : int2;
+    const end = (start === int1) ? int2 : int1;
+    let sum = start;
 
+    for(let i = start + 1; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
